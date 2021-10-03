d608c8d03f9e05f981e66a3edd7272720e8fb75c
Douglas <dougsrodrigues@outlook.com>
2021-10-03 14:02:31 -0300

    feat: init categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-import express from "express";
+import express from 'express';
+
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.listen(3333, () => console.log("Serviço codando na porta 3333"));
+app.use(express.json());
+
+app.use(categoriesRoutes);
+
+app.listen(3333, () => console.log('Serviço rodando na porta 3333'));
