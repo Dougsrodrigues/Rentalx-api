157b44c1a2c9dac0765d4bfd3031801ec53b0fb7
Douglas <dougsrodrigues@outlook.com>
2021-10-30 11:40:12 -0300

    feat: create feature of image cars

---
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -0,0 +1,12 @@
+import { inject, injectable } from 'tsyringe';
+
+// @injectable()
+class UploadCarImageUseCase {
+  // constructor(
+  // @inject('NameRepository')
+  // private NameRepository: INameRepository
+  // ) {}
+  async execute(): Promise<void> {}
+}
+
+export { UploadCarImageUseCase };
